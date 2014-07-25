737df705acaef69c536de4606d6933f15a3876e1
Petrus on Dupond <pgr@jaxo.com>
2014-07-25 11:26:25 +0200

    Processing args was not set!

--- rexx/main.cpp
+++ rexx/main.cpp
@@ -1,7 +1,8 @@
 #include <string.h>
 #include <fstream>
-#include "../toolslib/SystemContext.h"
 #include "../toolslib/StdFileStream.h"
+#include "../toolslib/StringBuffer.h"
+#include "../toolslib/SystemContext.h"
 #include "../rexxlib/Rexx.h"
 
 #ifndef YAXX_NAMESPACE
@@ -16,34 +17,43 @@
 #error Bare REXX must deny XML extension
 #endif
 
-/*----------------------------------------------------------------------usage-+
-|                                                                             |
-+----------------------------------------------------------------------------*/
-static void usage() {
-   cout << "Usage: \trexx rexxFile\n" << endl;
-}
-
 /*-----------------------------------------------------------------------main-+
 |                                                                             |
 +----------------------------------------------------------------------------*/
 int main(int argc, char ** argv)
 {
-   YAXX_NAMESPACE::Rexx rexx;
 
-   // -- need a SystemContext, red priority!
-   char * fileBaseUri = TOOLS_NAMESPACE::StdFileStreamBuf::makeBaseUri();
-   TOOLS_NAMESPACE::SystemContext context(
-      fileBaseUri,
-      TOOLS_NAMESPACE::StdFileSchemeHandler()
-   );
-   free(fileBaseUri);
-
-   if (argc != 2) {
-      usage();
-      return -1;
+   if ((argc < 2) || (argv[1][0] == '?')) {
+      puts("\nsyntax: rexx rexxfilepath [args]");
+      return 0;
    }else {
-      YAXX_NAMESPACE::Rexx::Script script(argv[1]); // create the script
-      return rexx.interpret(script);
+      YAXX_NAMESPACE::Rexx rexx;
+      // -- need a SystemContext (singleton), red priority!
+      char * fileBaseUri = TOOLS_NAMESPACE::StdFileStreamBuf::makeBaseUri();
+      TOOLS_NAMESPACE::SystemContext context(
+         fileBaseUri,
+         TOOLS_NAMESPACE::StdFileSchemeHandler()
+      );
+      free(fileBaseUri);
+
+      // create the script
+      YAXX_NAMESPACE::Rexx::Script script(argv[1]);
+
+      // reassemble arguments for REXX if any
+      if (argc > 2) {
+         StringBuffer arg;
+         int i = 2;
+         for (;;) {
+            arg.append(argv[i], strlen(argv[i]));
+            if (++i >= argc) break;
+            arg.append(' ');
+         }
+         // run the argued script
+         return rexx.interpret(script, arg);
+      }else {
+         // run the bare script
+         return rexx.interpret(script);
+      }
    }
 }
 
